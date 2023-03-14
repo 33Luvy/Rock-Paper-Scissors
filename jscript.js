60eea39f351b079b017441d8af6e2c08b787530e
33Luvy <33luvy@gmail.com>
2023-03-14 17:34:27 +0000

    Added function for generating computer input

--- jscript.js
+++ jscript.js
@@ -1,76 +1,20 @@
-let computerChoice = undefined
-let playerChoice = undefined
-let playerScore = 0
-let computerScore = 0
+// Initalize the global variables
+let playerScore = 0;
+let computerScore = 0;
+let gameRounds = 0;
+let computerChoice
 
-// Function that generates computers input
-function getComputerChoice() {
-// Variable that lists the different choices
-    computerChoice = [
-    "Rock",
-    "Paper",
-    "Scissors"
-];
-// Variable that chooses the random choice
- computerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-}
-
-// Function for playing one round of RPS
-function playRound(computerSelection, playerSelection) {
-if (playerSelection == ("rock") && computerChoice == ("Scissors")) {
-console.log("You win")
-playerScore = playerScore + 1;
-}
-
-else if (playerSelection == ("rock") && computerChoice == ("Paper")) {
-console.log("You lose")
-computerScore = computerScore + 1;
-}
-
-else if (playerSelection == ("rock") && computerChoice == ("Rock")) {
-console.log("It's a draw!")
-}
-
-else if (playerSelection == ("scissors") && computerChoice == ("Paper")) {
-console.log("You win")
-playerScore = playerScore + 1;
-}
-
-else if (playerSelection == ("scissors") && computerChoice == ("Rock")) {
-console.log("You lose")
-computerScore = computerScore + 1;
-}
-
-else if (playerSelection == ("scissors") && computerChoice == ("Scissors")) {
-console.log("It's a draw!")
-}
-
-else if (playerSelection == ("paper") && computerChoice == ("Rock")) {
-console.log("You win")
-playerScore = playerScore + 1;
-}
-
-else if (playerSelection == ("paper") && computerChoice == ("Scissors")) {
-console.log("You lose")
-computerScore = computerScore + 1;
-}
-
-else if (playerSelection == ("paper") && computerChoice == ("Paper")) {
-console.log("It's a draw!")
-}
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerChoice = prompt("Rock, Paper or Scissors?")
-        playRound(computerSelection, playerSelection)
-        const computerSelection = getComputerChoice();
-        const playerSelection = playerChoice
+// Function for computer choice
+function getComputerChoice(computerChoice) {
+    // Variable that generates a number 0 - 3
+    let randomChoice = Math.floor(Math.random() * 3);
+    if (randomChoice == 1) {
+        computerChoice = ("rock")
+    }
+    else if (randomChoice == 2) {
+        computerChoice = ("Paper")
+    }
+    else {
+        computerChoice = ("Scissors")
     }
 }
-
-
-
-
-
-game();
