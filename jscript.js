f5661def87beb072d868a7718486549eb5bcfa6a
33Luvy <33luvy@gmail.com>
2023-03-14 19:30:09 +0000

    Added the function to play one round of RPS

--- jscript.js
+++ jscript.js
@@ -5,16 +5,44 @@ let gameRounds = 0;
 let computerChoice
 
 // Function for computer choice
-function getComputerChoice(computerChoice) {
-    // Variable that generates a number 0 - 3
-    let randomChoice = Math.floor(Math.random() * 3);
-    if (randomChoice == 1) {
-        computerChoice = ("rock")
-    }
-    else if (randomChoice == 2) {
-        computerChoice = ("Paper")
-    }
-    else {
-        computerChoice = ("Scissors")
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+// Player and computer selection parameters
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+
+// Function for playing one round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+// IF statement for all possible winning conditions
+if ((playerSelection == "rock" && computerSelection == "Scissors") || 
+    (playerSelection == "paper" && computerSelection == "Rock") || 
+    (playerSelection == "scissors" && computerSelection == "Paper")) {
+    
+    playerScore += 1;
+
+    if (playerScore == 5) {
+        console.log("Player Wins!")
     }
 }
+// ELSE IF statement for all possible losing conditions
+else if ((playerSelection == "rock" && computerSelection == "Paper") || 
+        (playerSelection == "paper" && computerSelection == "Scissors") || 
+        (playerSelection == "scissors" && computerSelection == "Rock")) {
+    
+        computerScore += 1;
+
+        if (computerScore == 5) {
+            console.log("Computer Wins!")
+        }
+}
+// ELSE statement for all possible draw conditions
+else {
+    console.log("Both players have chosen",playerSelection,)
+}
+}
+
+
+playRound(playerSelection, computerSelection)
